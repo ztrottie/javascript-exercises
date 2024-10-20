38ea688a238118bbfa2eaa0af5319ab4b83c38c9
ztrottie <zakytrottier@hotmail.fr>
2024-10-20 19:25:51 -0400

    done the 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+	for (const arg of args) {
+		let index = array.indexOf(arg);
+		while (index >= 0) {
+			if (index > 0)
+				array = array.slice(0, index).concat(array.slice(index + 1));
+			else
+				array = array.slice(1);
+			index = array.indexOf(arg);
+		}
+	}
+	return array
 };
 
 // Do not edit below this line
