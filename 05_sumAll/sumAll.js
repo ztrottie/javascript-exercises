fec20c9dae3827c86d1270b731d6beb72197b3b3
ztrottie <zakytrottier@hotmail.fr>
2024-10-20 19:44:18 -0400

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstValue, secondValue) {
+	let total = 0;
+	if (firstValue < 0 || secondValue < 0 || !Number.isInteger(firstValue) || !Number.isInteger(secondValue))
+		return "ERROR"
+	let sumStart = (firstValue < secondValue) ? firstValue : secondValue;
+	let sumEnd = (secondValue > firstValue) ? secondValue : firstValue;
+	while (sumStart <= sumEnd) {
+		total += sumStart;
+		sumStart++;
+	}
+	return total;
 };
 
 // Do not edit below this line
