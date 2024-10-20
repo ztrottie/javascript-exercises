6f36ad0cc54bbd30b1332018a875c2219a7a2514
ztrottie <zakytrottier@hotmail.fr>
2024-10-20 17:28:20 -0400

    finished exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, amount) {
+	let finalString = '';
 
+	if (amount < 0)
+		return "ERROR"
+	for (let i = 0; i < amount; i++) {
+		finalString += string;
+	}
+	return finalString;
 };
 
 // Do not edit below this line
