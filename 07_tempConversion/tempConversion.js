968dc72d4d3a4da9c62e103ad53d56d5ef0fa4ba
ztrottie <zakytrottier@hotmail.fr>
2024-10-20 20:01:36 -0400

    done with 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Number(((fahrenheit - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return Number(((celcius * (9 / 5) + 32)).toFixed(1));
 };
 
 // Do not edit below this line
